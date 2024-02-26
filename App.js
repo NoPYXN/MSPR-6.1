cef5c2c666a931a82c83e5b54c31b24f6730c64f
Melly Lucas <melly.lucas32@yahoo.fr>
2024-02-26 17:59:49 +0100

    commencement getAnnonces

--- App.js
+++ App.js
@@ -1,30 +1,66 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Header from './src/components/header';
+import React, { useState, useEffect } from "react"
+import { StyleSheet, Text, View, Image } from "react-native"
+import Header from "./src/components/header"
+import axios from "axios"
 
 const App = () => {
-  return (
-    <View style={styles.container}>
-      <Header 
-        onMenuPress={() => console.log('Menu Pressed')}
-        onProfilePress={() => console.log('Profile Pressed')}
-      />
-    </View>
-  );
-};
+    const [annonces, setAnnonces] = useState([])
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:8080/api/v1/annonces")
+            .then(data => {
+                if (data.status == 200) {
+                    console.log(data.data.content)
+                    setAnnonces(data.data.content)
+                }
+            })
+            .catch(err => console.log(err))
+    }, [])
+    return (
+        <View>
+            {annonces.map(element => (
+                <View key={element.Id_Annonce}>
+                    <Image
+                        style={styles.imageAnnonce}
+                        source={{
+                            uri: element.Id_Plante[0],
+                        }}
+                    />
+                    <Text>{element.Titre}</Text>
+                    <Text>{element.Ville}</Text>
+                    <Text>Annonce postée le {element.DateCreation}</Text>
+                </View>
+            ))}
+        </View>
+        // <View style={styles.container}>
+        //   <Header
+        //     onMenuPress={() => console.log('Menu Pressed')}
+        //     onProfilePress={() => console.log('Profile Pressed')}
+        //   />
+        // </View>
+    )
+}
+
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         backgroundColor: "#fff",
+//         alignItems: "center",
+//         justifyContent: "center",
+//     },
+//     welcomeText: {
+//         fontSize: 20,
+//         textAlign: "center",
+//         margin: 10,
+//     },
+// })
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  welcomeText: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
+    imageAnnonce: {
+        width: 50,
+        height: 50,
+    },
+})
 
-export default App;
+export default App
