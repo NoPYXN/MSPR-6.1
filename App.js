06a605887715e08c2d93c8d4a3770e83d04a9a8b
Melly Lucas <melly.lucas32@yahoo.fr>
2024-02-27 17:36:27 +0100

    page annonce

--- App.js
+++ App.js
@@ -1,65 +1,217 @@
 import React, { useState, useEffect } from "react"
-import { StyleSheet, Text, View, Image } from "react-native"
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    FlatList,
+    SafeAreaView,
+    TouchableOpacity,
+} from "react-native"
 import Header from "./src/components/header"
 import axios from "axios"
+import { format, parseISO } from "date-fns"
+import { fr } from "date-fns/locale"
+import { FaMapMarkerAlt } from "react-icons/fa"
+import Input from "./src/components/input/input"
 
 const App = () => {
     const [annonces, setAnnonces] = useState([])
+    const [showFirstView, setShowFirstView] = useState(true)
+    const [calculPage, setCalculPage] = useState(0)
+    const [pageChoisie, setPageChoisie] = useState(0)
 
     useEffect(() => {
         axios
-            .get("http://localhost:8080/api/v1/annonces")
+            .get(`http://localhost:8080/api/v1/annonces?page=${pageChoisie}`)
             .then(data => {
                 if (data.status == 200) {
                     console.log(data.data.content)
+
+                    setCalculPage(Math.ceil(data.data.content.length / 4))
                     setAnnonces(data.data.content)
                 }
             })
             .catch(err => console.log(err))
+        const timer = setTimeout(() => {
+            setShowFirstView(false)
+        }, 2000)
+
+        return () => clearTimeout(timer)
     }, [])
-    return (
-        <View>
-            {annonces.map(element => (
-                <View key={element.Id_Annonce}>
-                    <Image
-                        style={styles.imageAnnonce}
-                        source={{
-                            uri: element.Id_Plante[0],
-                        }}
-                    />
-                    <Text>{element.Titre}</Text>
-                    <Text>{element.Ville}</Text>
-                    <Text>Annonce postée le {element.DateCreation}</Text>
-                </View>
-            ))}
+
+    const boutons = Array.from({ length: calculPage }, (_, index) => (
+        <View style={styles.ViewButton} key={index}>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => {
+                    changePage(index)
+                }}
+            >
+                <Text>{index + 1}</Text>
+            </TouchableOpacity>
         </View>
-        // <View style={styles.container}>
-        //   <Header
-        //     onMenuPress={() => console.log('Menu Pressed')}
-        //     onProfilePress={() => console.log('Profile Pressed')}
-        //   />
-        // </View>
+    ))
+
+    const changePage = index => {
+        axios
+            .get(`http://localhost:8080/api/v1/annonces?page=${index}`)
+            .then(data => {
+                if (data.status == 200) {
+                    // console.log(data.data.content)
+
+                    // setCalculPage(Math.ceil(data.data.content.length / 4))
+                    setAnnonces(data.data.content)
+                }
+            })
+            .catch(err => console.log(err))
+    }
+
+    return (
+        <SafeAreaView style={styles.SafeAreaView}>
+            <View style={styles.ViewHeader}></View>
+            {showFirstView ? (
+                <View style={styles.container}>
+                    <Header
+                        onMenuPress={() => console.log("Menu Pressed")}
+                        onProfilePress={() => console.log("Profile Pressed")}
+                    />
+                </View>
+            ) : (
+                <View style={styles.ViewGlobale}>
+                    <View style={styles.ViewLocalisation}>
+                        <Text>Se localiser </Text>
+                        <View>
+                            <FaMapMarkerAlt />
+                        </View>
+                    </View>
+                    <View style={styles.ViewSearchAnnonces}>
+                        <Text style={styles.SearchVille}>Chercher une ville</Text>
+                        <Input type="search" />
+                        {annonces ? (
+                            <View style={styles.ViewAnnonces}>
+                                {annonces.map(item => (
+                                    <View key={item.Id_Annonce} style={styles.ViewAnnonce}>
+                                        <Image
+                                            style={styles.imageAnnonce}
+                                            source={{
+                                                uri: item.Id_Plante[0],
+                                            }}
+                                        />
+                                        <View style={styles.infoAnnonce}>
+                                            <Text style={styles.titreAnnonce}>{item.Titre}</Text>
+                                            <Text style={styles.villeAnnonce}>{item.Ville}</Text>
+                                            <Text style={styles.dateAnnonce}>
+                                                Depuis le{" "}
+                                                {format(
+                                                    parseISO(item.DateCreation),
+                                                    "EEEE dd MMMM yyyy",
+                                                    {
+                                                        locale: fr,
+                                                    },
+                                                )}
+                                            </Text>
+                                        </View>
+                                    </View>
+                                ))}
+                            </View>
+                        ) : (
+                            <View>Aucune annonce trouvée</View>
+                        )}
+                    </View>
+
+                    <View style={styles.ViewButtons}>
+                        {annonces && annonces.length > 0 ? boutons : <Text>Pas d'annonces</Text>}
+                    </View>
+                </View>
+            )}
+        </SafeAreaView>
     )
 }
 
-// const styles = StyleSheet.create({
-//     container: {
-//         flex: 1,
-//         backgroundColor: "#fff",
-//         alignItems: "center",
-//         justifyContent: "center",
-//     },
-//     welcomeText: {
-//         fontSize: 20,
-//         textAlign: "center",
-//         margin: 10,
-//     },
-// })
-
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    welcomeText: {
+        fontSize: 20,
+        textAlign: "center",
+        margin: 10,
+    },
+    SafeAreaView: {
+        width: "100%",
+    },
+    ViewGlobale: {
+        margin: "5%",
+    },
+    SearchVille: {
+        textAlign: "center",
+        marginBottom: "2%",
+    },
     imageAnnonce: {
-        width: 50,
-        height: 50,
+        width: 100,
+        height: 100,
+        borderRadius: 10,
+        marginBottom: 10,
+    },
+    ViewSearchAnnonces: {
+        marginTop: "7%",
+    },
+    ViewAnnonce: {
+        flexDirection: "row",
+        padding: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: "#E0E0E0",
+        width: "100%",
+    },
+    ViewAnnonces: {
+        flexDirection: "column",
+        width: "90%",
+        marginLeft: "5%",
+        marginRight: "5%",
+        marginTop: "7%",
+    },
+    infoAnnonce: {
+        flexDirection: "column",
+        justifyContent: "center",
+        marginLeft: "5%",
+        flex: 1,
+    },
+    titreAnnonce: {
+        fontSize: 18,
+        fontWeight: "bold",
+        marginBottom: 5,
+    },
+    villeAnnonce: {
+        fontSize: 14,
+        color: "#29771D",
+        fontWeight: "bold",
+        marginBottom: 5,
+    },
+    dateAnnonce: {
+        fontSize: 10,
+        color: "#757575",
+        textAlign: "right",
+    },
+    ViewLocalisation: {
+        display: "flex",
+        flexDirection: "row",
+    },
+    ViewHeader: {
+        width: "100%",
+        height: "10%",
+        backgroundColor: "#29771D",
+    },
+    ViewButtons: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+    },
+    ViewButton: {
+        margin: "5%",
     },
 })
 
--- src/components/header.js
+++ src/components/header.js
@@ -1,43 +1,42 @@
-import React from 'react';
-import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import React from "react"
+import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native"
 import LogoArosaje from "../assets/logo.png"
 
 const Header = ({ onMenuPress, onProfilePress }) => {
     return (
-<View style={styles.headerContainer}>
-    {/* Menu Hamburger */}
-    <TouchableOpacity onPress={onMenuPress}>
-        <Image
-            source={LogoArosaje} // Remplacez par votre icône de menu
-            style={styles.icon}
-        />
-    </TouchableOpacity>
+        <View style={styles.headerContainer}>
+            {/* Menu Hamburger */}
+            <TouchableOpacity onPress={onMenuPress}>
+                <Image
+                    source={LogoArosaje} // Remplacez par votre icône de menu
+                    style={styles.icon}
+                />
+            </TouchableOpacity>
 
-    {/* Logo */}
-    <Image
-        source={LogoArosaje} // Remplacez par votre logo
-        style={styles.logo}
-    />
+            {/* Logo */}
+            <Image
+                source={LogoArosaje} // Remplacez par votre logo
+                style={styles.logo}
+            />
 
-    {/* Bouton Profil */}
-    <TouchableOpacity onPress={onProfilePress}>
-        <Image
-            source={LogoArosaje} // Remplacez par votre icône de profil
-            style={styles.icon}
-        />
-    </TouchableOpacity>
-</View>
-
-    );
-};
+            {/* Bouton Profil */}
+            <TouchableOpacity onPress={onProfilePress}>
+                <Image
+                    source={LogoArosaje} // Remplacez par votre icône de profil
+                    style={styles.icon}
+                />
+            </TouchableOpacity>
+        </View>
+    )
+}
 
 const styles = StyleSheet.create({
     headerContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
         padding: 10,
-        backgroundColor: '#fff', // Modifiez selon votre thème
+        backgroundColor: "#fff", // Modifiez selon votre thème
     },
     icon: {
         width: 25,
@@ -46,8 +45,8 @@ const styles = StyleSheet.create({
     logo: {
         width: 100,
         height: 40,
-        resizeMode: 'contain',
+        resizeMode: "contain",
     },
-});
+})
 
-export default Header;
+export default Header
