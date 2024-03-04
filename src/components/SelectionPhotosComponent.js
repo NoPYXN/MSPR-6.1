683ee37eac44e0d012d9f1d5f52c7be499176700
NoP_YXN <yunfournes@gmail.com>
2024-03-04 23:45:05 +0100

    Page d'annonce update

--- src/components/SelectionPhotosComponent.js
+++ src/components/SelectionPhotosComponent.js
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { View, Image, TouchableOpacity, Text, StyleSheet } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { FontAwesome5 } from '@expo/vector-icons';
+import Galerie from './GalerieComponent';
 
 const PhotoPicker = ({ onImageSelect }) => {
-  const [selectedImage, setSelectedImage] = useState(null);
+  const [selectedImages, setSelectedImages] = useState([]);
 
   useEffect(() => {
     (async () => {
@@ -24,7 +25,7 @@ const PhotoPicker = ({ onImageSelect }) => {
     });
 
     if (!result.cancelled) {
-      setSelectedImage(result.uri);
+      setSelectedImages([...selectedImages, result.uri]);
       // Appel de la fonction de rappel avec l'URI de l'image sélectionnée
       onImageSelect(result.uri);
     }
@@ -32,13 +33,13 @@ const PhotoPicker = ({ onImageSelect }) => {
 
   return (
     <View style={styles.container}>
+      {selectedImages.length > 0 && <Galerie images={selectedImages} />}
+      
       <TouchableOpacity style={styles.button} onPress={pickImage}>
         <FontAwesome5 name="upload" size={50} color="black" />
         <Text style={styles.text}>Ajouter des photos</Text>
       </TouchableOpacity>
-      {selectedImage && (
-        <Image source={{ uri: selectedImage }} style={styles.image} />
-      )}
+      
     </View>
   );
 };
@@ -54,16 +55,11 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: 'gray',
     borderRadius: 5,
-    width: 200,
-    height: 150,
+    width: 150,
+    height: 100,
     justifyContent: 'center',
     alignItems: 'center',
     margin: 20,
-  },
-  image: {
-    width: 200,
-    height: 200,
-    marginTop: 20,
   },
   text: {
     marginTop: 10,
--- src/screens/AnnonceScreen.js
+++ src/screens/AnnonceScreen.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react"
-import { StyleSheet, Text, View, TextInput, SafeAreaView} from "react-native"
-import { useNavigation} from "@react-navigation/native"
+import { StyleSheet, Text, View, TextInput, SafeAreaView } from "react-native"
+import { useNavigation } from "@react-navigation/native"
+import { Ionicons } from '@expo/vector-icons'; // Import Ionicons from expo
 
 import HeaderComponent from "../components/HeaderComponent"
 import Carousel from "../components/CarrouselComponent"
@@ -8,29 +9,29 @@ import PhotoPicker from "../components/SelectionPhotosComponent"
 import TextZoneInfo from "../components/TextZoneInfoComponent"
 
 const AnnonceScreen = () => {
-    
+
     const navigation = useNavigation()
 
-    // const logo = "https://res.cloudinary.com/melly-lucas/image/upload/v1704971723/Arosaje/annonces/plante_kqt4sg.avif"
-    // const profil = "https://res.cloudinary.com/melly-lucas/image/upload/v1704971723/Arosaje/annonces/plante_kqt4sg.avif"
-    // const favicon = "https://res.cloudinary.com/melly-lucas/image/upload/v1704971723/Arosaje/annonces/plante_kqt4sg.avif"
-    // const images = [logo, profil, favicon];
-
     const [images, setImages] = useState([])
+    const [messages, setMessages] = useState([])
 
-    useEffect(() => {setImages(["https://res.cloudinary.com/melly-lucas/image/upload/v1704971723/Arosaje/annonces/plante_kqt4sg.avif", "https://res.cloudinary.com/melly-lucas/image/upload/v1704971723/Arosaje/annonces/plante_kqt4sg.avif", "https://res.cloudinary.com/melly-lucas/image/upload/v1704971723/Arosaje/annonces/plante_kqt4sg.avif"])},[])
+    useEffect(() => {
+        setImages(["https://res.cloudinary.com/melly-lucas/image/upload/v1704971723/Arosaje/annonces/plante_kqt4sg.avif", "https://res.cloudinary.com/melly-lucas/image/upload/v1704971723/Arosaje/annonces/plante_kqt4sg.avif", "https://res.cloudinary.com/melly-lucas/image/upload/v1704971723/Arosaje/annonces/plante_kqt4sg.avif"])
+    }, [])
 
     const handleSubmission = (text) => {
-        // Logique de soumission ou d'envoi des données du texte
+        setMessages([...messages, {text, time: new Date()}]) // Store text and time
         console.log('Text submitted:', text);
     };
+
+    const [selectedImage, setSelectedImage] = useState(null);
+
     const handleImageSelect = (imageUri) => {
-        // Logique à exécuter lorsque l'image est sélectionnée
+        setSelectedImage(imageUri);
         console.log('Image selected:', imageUri);
-        // Vous pouvez stocker l'URI de l'image dans un état ou effectuer toute autre opération nécessaire
     };
 
-    return ( 
+    return (
         <SafeAreaView style={styles.SafeAreaView}>
             <HeaderComponent navigation={navigation} />
             <Text style={styles.TextCenter}>Nom de la plante</Text>
@@ -48,15 +49,29 @@ const AnnonceScreen = () => {
                 </View>
             </View>
             <View style={styles.separateur}></View>
-            <Text style={styles.TextModule}>Module de tchat (même visuel...)</Text>
-            <View style={styles.separateur}></View>
+            
             <PhotoPicker onImageSelect={handleImageSelect} />
             <View style={styles.separateur}></View>
-            <Text style={styles.TextIndication}>Avez-vous des indications a transmettre ?</Text>
+            <View style={styles.messageContainer}>
+                {messages.map((message, index) => (
+                    <View key={index} style={styles.message}>
+                        <View style={styles.avatarContainer}>
+                            <Ionicons name="person-circle-outline" size={24} color="black" />
+                            <Text style={styles.pseudo}>Anonyme</Text>
+                        </View>
+                        <View style={styles.messageContent}>
+                            <Text style={styles.messageText}>{message.text}</Text>
+                            <Text style={styles.messageTime}>{message.time.toLocaleString()}</Text>
+                        </View>
+                    </View>
+                ))}
+            </View>
+            <Text style={styles.TextIndication}>Avez-vous des indications à transmettre ?</Text>
             <TextZoneInfo onSubmit={handleSubmission} />
         </SafeAreaView>
     )
 }
+
 const styles = StyleSheet.create({
     TextCenter: {
         textAlign: "center",
@@ -108,5 +123,42 @@ const styles = StyleSheet.create({
         fontSize: 24,
         paddingVertical: 70,
     },
+    messageContainer: {
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        marginHorizontal: 20,
+        marginTop: 10,
+        
+    },
+    message: {
+        flexDirection: "row",
+        marginBottom: 10,
+    },
+    avatarContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        marginRight: 10,
+    },
+    pseudo: {
+        marginLeft: 5,
+        fontWeight: "bold",
+    },
+    messageContent: {
+        flex: 1,
+        borderWidth: 1,
+        borderColor: "#ccc",
+        borderRadius: 10,
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+    },
+    messageText: {
+        fontSize: 16,
+    },
+    messageTime: {
+        fontSize: 12,
+        color: "#888",
+        textAlign: "right",
+    },
 })
+
 export default AnnonceScreen
